a620ce0d6bf8de51776186e89dbf50a7b22df7d8
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-04 08:43:05 -0300

    024 - endpoint para obter repos; componentes Repos e RepoItem; repos como props em User

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,21 @@
 import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import Repos from '../repos/Repos'
 import Spinner from '../layout/Spinner'
 import { Link } from 'react-router-dom'
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   }
 
   render() {
@@ -24,7 +35,7 @@ export class User extends Component {
       hireable
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />
 
@@ -78,6 +89,7 @@ export class User extends Component {
           <div className='badge badge-light'>Repositórios públicos: {public_repos}</div>
           <div className='badge badge-dark'>Gists públicos: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     )
   }
